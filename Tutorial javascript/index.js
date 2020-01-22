7a764c1bf26f156302bb2f25612ef056694737d0
Albert Cynarski <ALBCY@statoilfuelretail.com>
2020-01-22 10:25:34 +0100

    Dodanie stronki

---
+++ Tutorial javascript/index.js
@@ -0,0 +1,26 @@
+var number = 5; // in line comment
+
+/* comment fhdlka
+hfkdha
+
+f*/
+
+var myName = "Bob"
+
+myName = 8
+
+let ourName = "freeCodeCamp"
+
+const pi = 3.14
+
+### 
+
+var a;
+var b = 2;
+
+a = 7;
+
+console.log(a)
+
+console.log("Hello World")
+
